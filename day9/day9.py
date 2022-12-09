a81b8bcc14f97bbb271dead06c2f623dc5bd9e24
sm-moore <sara.moore@recursionpharma.com>
2022-12-09 00:51:19 -0700

    cleaning up code

--- day9/day9.py
+++ day9/day9.py
@@ -1,21 +1,3 @@
-# class Node:
-#     top = None
-#     left = None
-#     right = None
-#     bottom = None
-#     tail_visits = 0
-
-#     def __init__(self, left, top, right, bottom):
-#         self.top = top
-#         self.left = left
-#         self.right = right
-#         self.bottom = bottom
-
-#     # def __eq__(self, other):
-#     # """Overrides the default implementation"""
-#     # return other.top == self.top and other.left == self.left and 
-
-
 def txt_to_lines():
     with open('day9/input.txt') as f:
         lines = f.read().splitlines()
@@ -23,28 +5,12 @@ def txt_to_lines():
 
 
 def parse(lines):
-    # 'R 4',
-    # 'U 4',
-    # 'L 3',
-    # 'D 1',
-    # 'R 4',
-    # 'D 1',
-    # 'L 5',
-    # 'R 2'
-
-    # ..##..
-    # ...##.
-    # .####.
-    # ....#.
-    # s###..
-
     # What if I just kept a list of x,y coords the tail touched?
     result = set([(0, 0)])
     head_coords = (0, 0)
     tail_coords = (0, 0)
     for line in lines:
         direction, n = line.split(' ')
-        # breakpoint()
         for _ in range(int(n)):
             head_coords, tail_coords = move_one(direction, head_coords, tail_coords)
             result.add(tail_coords)
@@ -69,12 +35,6 @@ def touching(tail_coords, new_head_coords):
 
 def move_one(direction, head_coords, tail_coords):
     # Returns new tail_coords and head_coords
-    # ..##..
-    # ...##.
-    # .####.
-    # ....#.
-    # s###.. X (3, 0) -> (4,1) This is touching. (3, 0) -> (5,1) not touching
-    # Y 
     new_tail_coords = tail_coords
     match direction:
         case 'R':
@@ -94,6 +54,7 @@ def move_one(direction, head_coords, tail_coords):
 
     if not touching(tail_coords, new_head_coords):
         # Move tail to where the head used to be.
+        # NOTE!!! THIS IS WRONG!!! UGH!!!
         new_tail_coords = head_coords
     return new_head_coords, new_tail_coords
 
